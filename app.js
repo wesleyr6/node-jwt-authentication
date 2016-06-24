1683b42a0fbd6c1d2431309cb09308694f1c4715
Wesley Amaro <maz_wamaro@uolinc.com>
2016-06-24 19:53:54 -0300

    Config express-session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var exphbs = require('express3-handlebars');
+var expressSession = require('express-session');
 var mongoose = require('mongoose');
 var config = require('./config');
 var routes = require('./routes/index');
@@ -16,8 +17,21 @@ mongoose.connect(config.database, function(err) {
 	console.log('MongoDB: Successfully connected');
 });
 
+// Views
+app.use('/static', express.static(__dirname + '/static'));
+app.use('/views', express.static(__dirname + '/views'));
+
 // Configs
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+app.use(expressSession({
+	secret: process.env.SESSION_SECRET || 'safadao',
+	resave: false,
+	saveUninitialized: false
+}));
 
 // Template engine
 app.engine('hbs', exphbs({
@@ -25,16 +39,6 @@ app.engine('hbs', exphbs({
 	defaultLayout: 'unlogged/main.hbs'
 }));
 app.set('view engine', 'hbs');
-
-// Views
-app.use('/static', express.static(__dirname + '/static'));
-app.use('/views', express.static(__dirname + '/views'));
-
-// To parse request params in req.body json format
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
 
 // Routes
 app.use('/', routes);
